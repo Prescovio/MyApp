3f58fcfcff129292ea7c093ecfc5c42bb1db8049
asd <asd>
2018-04-15 20:49:23 +0200

    added missing files

---
+++ app/src/main/java/com/example/leon/myapp/Views/Main/Practice/NavigationDrawer/NavigationDrawerActivity.java
@@ -0,0 +1,99 @@
+package com.example.leon.myapp.Views.Main.Practice.NavigationDrawer;
+
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.leon.myapp.*;
+import com.example.leon.myapp.Views.Login.LoginActivityView;
+import com.example.leon.myapp.Views.Registration.RegistrationActivityView;
+
+public class NavigationDrawerActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        drawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.nav_mercury:
+                                Toast.makeText(getBaseContext(), getString(R.string.mercury), Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.nav_venus:
+                                Toast.makeText(getBaseContext(), getString(R.string.venus), Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.nav_earth:
+                                Toast.makeText(getBaseContext(), getString(R.string.earth), Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.nav_mars:
+                                Toast.makeText(getBaseContext(), getString(R.string.mars), Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+
+                        return true;
+                    }
+                });
+
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher_round);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.logout:
+                Intent broadcastIntent = new Intent();
+                broadcastIntent.setAction("com.package.ACTION_LOGOUT");
+                sendBroadcast(broadcastIntent);
+
+                startActivity(new Intent(this, LoginActivityView.class));
+
+                break;
+            case R.id.mika:
+                startActivity(new Intent(this, RegistrationActivityView.class));
+
+                break;
+            default:
+                super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+}
